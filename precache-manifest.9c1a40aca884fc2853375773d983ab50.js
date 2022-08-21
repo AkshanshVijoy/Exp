1e71802faecb53a8a86838376f7818860f5ea180
Akshansh Vijoy <jumbojoy.2k@gmail.com>
2022-08-22 02:24:07 +0530

    Updates

---
+++ precache-manifest.9c1a40aca884fc2853375773d983ab50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e155274940e1585c751",
+    "url": "/exp/static/css/main.1204b6bb.chunk.css"
+  },
+  {
+    "revision": "4e155274940e1585c751",
+    "url": "/exp/static/js/main.4e155274.chunk.js"
+  },
+  {
+    "revision": "3efd5b7d8d4719b459d3",
+    "url": "/exp/static/js/1.3efd5b7d.chunk.js"
+  },
+  {
+    "revision": "2f002e673f4e7acd7cc8",
+    "url": "/exp/static/js/runtime~main.2f002e67.js"
+  },
+  {
+    "revision": "45e1f5e2a4e300f2b3cbacb1220e5abc",
+    "url": "/exp/index.html"
+  }
+];
